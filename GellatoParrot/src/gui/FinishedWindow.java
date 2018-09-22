1f4e94a6b05151d8f8a48eba4869c2695c180bcc
Unknown <maria2000jose.mj@gmail.com>
2018-09-22 01:40:39 -0600

    PopUp Finished Window

--- GellatoParrot/src/gui/FinishedWindow.java
+++ GellatoParrot/src/gui/FinishedWindow.java
@@ -1,30 +1,13 @@
 package gui;
 
-import java.awt.Color;
-import java.awt.Font;
+import javax.swing.JOptionPane;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+public class FinishedWindow {
 
-import consts.IGUIconsts;
-
-public class FinishedWindow extends JFrame implements IGUIconsts {
-	private JLabel StatusText;
-
-	public FinishedWindow() {
-		this.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-		this.setBackground(Color.decode("#abb2bc"));
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		initComponents();
-		this.setLocationRelativeTo(null);
-		this.setVisible(true);
-	}
-
-	public void initComponents() {
-		StatusText = new JLabel("YOU WON!");
-		StatusText.setFont(new Font("Arial", Font.BOLD, 70));
-		StatusText.setLocation(WINDOW_WIDTH / 2, this.getHeight() / 2 - 50);
-		StatusText.setSize(200, 100);
-		this.getContentPane().add(StatusText);
-	}
+    public static void infoBox(String infoMessage, String titleBar)
+    {  
+        JOptionPane.showMessageDialog(null, infoMessage, "InfoBox: " + titleBar, JOptionPane.INFORMATION_MESSAGE);
+    }
+    
+    
 }
